6faf94d4ece3a320d1c216261c5545167e60277b
bbakbum <zxc534@naver.com>
2023-05-29 18:54:56 +0900

    heap sort complete

--- Tool/HeapSort.c
+++ Tool/HeapSort.c
@@ -4,18 +4,25 @@
 
 #define MAX_HEAP_SIZE 50
 
-int initHeap(int * heap, int * index, int * height);
+void initHeap(int * heap, int * index, int * height);
+void sortHeap(int * heap, int size);
 void printHeap(int * heap, int index, int height);
+
+void swap(int * a, int * b);
+
+void drop(int * heap, int size, int index);
+void pop(int * heap, int size);
 
 int main (void) {
     int * heap = (int*)malloc(sizeof(int) * MAX_HEAP_SIZE);
-    int index = 1;
+    int size = 1;
     int height = 0;
-    initHeap(heap, &index, &height);
-    //printHeap(heap, index, height);
+    initHeap(heap, &size, &height);
+    sortHeap(heap, size);
+    printHeap(heap, size, height);
 }
 
-int initHeap(int * heap, int * index, int * height){
+void initHeap(int * heap, int * size, int * height){
     int i,j,N;
     int jEnd;
     //1.Input
@@ -34,18 +41,67 @@ int initHeap(int * heap, int * index, int * height){
     for(i=1; i<=11; i++){
         heap[i] = arr[i-1];
     }
-    *index = 12;
-    *height = (int)(log(*index)/log(2));
+    *size = 12;
+    *height = (int)(log(*size)/log(2));
 
     printf("Initiailizing heap...");
     for(i=*height;i>0;i--){
         j = (int)pow(2, i-1);
         jEnd = (int)pow(2, i);
-        for(;(j<*index)&&(j<jEnd);j++){
-            printf("%d ",heap[j]);
+        for(;(j<*size)&&(j<jEnd);j++){
+            //printf("%d ",heap[j]);
+            //compare every node with its child node
+            drop(heap, *size, j);
         }
     }
+}
 
+void sortHeap(int * heap, int size){
+    for(int i=1;i<size;i++){
+        pop(heap,size-i);
+    }
+    
+}
+
+void swap(int * a, int * b){
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void drop(int * heap, int size ,int index){
+    int temp;
+    int lvalue=0;
+    int rvalue=0;
+    if(index*2<size){     //left child node exist
+        lvalue = heap[index*2];
+    }
+    if(index*2+1<size){     //right child node exist
+        rvalue = heap[index*2+1];
+    }
+    if(rvalue > lvalue){
+        if(rvalue > heap[index]){
+            swap(&heap[index], &heap[index*2+1]);
+            drop(heap, size, index*2+1);
+            return;
+        }
+    } else {
+        if(lvalue > heap[index]){
+            swap(&heap[index], &heap[index*2]);
+            drop(heap, size, index*2);
+            return;
+        }
+    }
+}
+
+void pop(int * heap, int size){
+    int temp;
+
+    temp = heap[1];
+    heap[1] = heap[size];
+    heap[size] = temp;
+    drop(heap,size,1);
 }
 
 void printHeap(int * heap, int index, int height){
